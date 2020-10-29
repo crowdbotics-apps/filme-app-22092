0eaed19f530988d65b13bd53d2a2d4135702b689
Crowdbotics <team@crowdbotics.com>
2020-10-29 17:37:27 +0000

    Initial commit.

---
+++ backend/filme_app_22092/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for filme_app_22092 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'filme_app_22092.settings')
+
+application = get_wsgi_application()
